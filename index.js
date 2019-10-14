2cc0b31e6ce36c0de9b9329537e92c741fc53db7
Kev Comedia <kevcomedia@gmail.com>
2019-10-14 21:52:43 +0800

    Move timestamp logic to own module

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
 const express = require('express')
+const toTimestampJson = require('./toTimestampJson')
 const app = express()
-
-function toTimestampJson(dateString) {
-  let dateArg
-
-  if (!dateString) {
-    dateArg = null
-  } else if (/^\d+$/.test(dateString)) {
-    // UNIX timestamp with milliseconds
-    dateArg = +dateString
-  } else {
-    // ISO 8601 date format is YYYY-MM-DD; 10 characters.
-    // We'll ignore subsequent characters.
-    dateArg = dateString.slice(0, 10)
-  }
-
-  const date = new Date(dateArg)
-  const unix = date.getTime()
-  const utc = date.toUTCString()
-
-  if (utc == 'Invalid Date') {
-    return {error: utc}
-  } else {
-    return { unix, utc }
-  }
-}
 
 app.set('view engine', 'pug')
 
