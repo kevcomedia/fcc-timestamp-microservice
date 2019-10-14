5ac893c592038d39677ff7713ca77051af0d4acc
Kev Comedia <kevcomedia@gmail.com>
2019-10-14 20:22:35 +0800

    Implement timestamp

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 const express = require('express')
 const app = express()
 
+function toTimestampJson(dateString) {
+  let dateArg
+
+  if (!dateString) {
+    dateArg = null
+  } else if (/^\d+$/.test(dateString)) {
+    // UNIX timestamp with milliseconds
+    dateArg = +dateString
+  } else {
+    // ISO 8601 date format is YYYY-MM-DD; 10 characters.
+    // We'll ignore subsequent characters.
+    dateArg = dateString.slice(0, 10)
+  }
+
+  const date = new Date(dateArg)
+  const unix = date.getTime()
+  const utc = date.toUTCString()
+
+  if (utc == 'Invalid Date') {
+    return {error: utc}
+  } else {
+    return { unix, utc }
+  }
+}
+
 app.get('/', (request, response) => {
   response.send('Hello world')
+})
+
+app.get('/api/timestamp', (request, response) => {
+  response.json(toTimestampJson())
+})
+
+app.get('/api/timestamp/:dateString', (request, response) => {
+  const { dateString } = request.params
+  response.json(toTimestampJson(dateString))
 })
 
 const port = process.env.PORT || 3000
